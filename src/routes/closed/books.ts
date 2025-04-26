07f14f767d6a6ed4f2864d007dabd4eda8e142b7
tianp1n <pinedchr1@gmail.com>
2025-04-25 21:58:21 -0700

    added fetch book by ISBN route

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -20,7 +20,31 @@ function mwValidISBN13(
 }
 
 bookRouter.get('/:isbn', (request: Request, response: Response) => {
-    // etc.
+    const query =
+        'SELECT isbn13, authors, publication_year, original_title, title, rating_1, rating_2, rating_3, rating_4, rating_5, image_url, image_small_url FROM BOOKS JOIN BOOKAUTHORS ON BOOKS.id = BOOKAUTHORS.id JOIN RATINGS ON BOOKS.id = RATINGS.id WHERE isbn13 = $1;';
+    const values = [request.params.name];
+
+    mwValidISBN13;
+    pool.query(query, values)
+        .then((result) => {
+            if (result.rowCount == 1) {
+                response.send({
+                    entry: result.rows[0],
+                });
+            } else {
+                response.status(404).send({
+                    message: 'ISBN not found',
+                });
+            }
+        })
+        .catch((error) => {
+            //log the error
+            console.error('DB Query error on GET /:name');
+            console.error(error);
+            response.status(500).send({
+                message: 'server error - contact support',
+            });
+        });
 });
 
 export { bookRouter };
