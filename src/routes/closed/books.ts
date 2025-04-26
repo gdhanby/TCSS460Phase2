83e0ae78cdc3a77a3f04bc6ffe52cc5455a1e2d1
Noah <noah.dangerfield@gmail.com>
2025-04-25 21:46:58 -0700

    added middleware function for checking provided isbn in route params.

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -3,6 +3,22 @@ import { pool, validationFunctions } from '../../core/utilities';
 
 const bookRouter: Router = express.Router();
 
+function mwValidISBN13(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const isbnString: string = request.params.isbn;
+    if (isbnString.length == 13 && /^\d+$/.test(isbnString)) {
+        next();
+    } else {
+        response.status(400).send({
+            message:
+                'Invalid or missing ISBN13 - please refer to documentation',
+        });
+    }
+}
+
 bookRouter.get('/:isbn', (request: Request, response: Response) => {
     // etc.
 });
