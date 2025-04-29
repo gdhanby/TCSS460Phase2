3ae4da3128711444f2bb2e10ad9a6d17751bbd68
Noah <noah.dangerfield@gmail.com>
2025-04-28 20:39:33 -0700

    Added stub for 'updating ratings of book' path

--- src/routes/closed/books.ts
+++ src/routes/closed/books.ts
@@ -353,4 +353,15 @@ bookRouter.post(
     }
 );
 
+bookRouter.patch(
+    '/:isbn13',
+    mwValidISBN13,
+    (request: Request, response: Response) => {
+        /* Route for updating rating counts of book. user provides isbn13 as route parameter, 
+    and request body has which counts they want to update (rating_1, rating_2, etc.). They
+    should be able to provide only the ones they want to update, and the value in the DB will
+    be overwritten. TODO: validation of new rating counts in body.*/
+    }
+);
+
 export { bookRouter };
